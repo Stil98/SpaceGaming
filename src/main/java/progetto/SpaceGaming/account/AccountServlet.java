c209526b6e2db5ed69db20a897b3108be341608b
Stil98 <sabamilan98@gmail.com>
2021-06-20 14:42:24 +0200

    Fix

---
+++ src/main/java/progetto/SpaceGaming/account/AccountServlet.java
@@ -0,0 +1,54 @@
+package progetto.SpaceGaming.account;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AccountServlet", value = "/account/*")
+public class AccountServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        String path= getPath(request);
+        switch (path){
+            case "/":
+                break;
+            case "/create":
+                break;
+            case "/secret":  //login admin
+                request.getRequestDispatcher("/WEB-INF/views/crm/secret.jsp").forward(request, response);
+                break;
+            case "/header":
+                request.getRequestDispatcher("/WEB-INF/views/partials/header.jsp").forward(request, response);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        String path= getPath(request);
+        switch (path){
+            case "/":
+                break;
+            case "/create":
+                break;
+            case "/secret":
+                break;
+            default:
+                break;
+        }
+    }
+
+    private String getPath(HttpServletRequest request) {
+        String path= (request.getPathInfo() != null) ? request.getPathInfo() : "/";
+        return path;
+    }
+
+}
+
---
+++ src/main/java/progetto/SpaceGaming/cart/CartItem.java
@@ -0,0 +1,27 @@
+package progetto.SpaceGaming.cart;
+
+import progetto.SpaceGaming.product.Product;
+
+public class CartItem {
+
+    private final Product product;
+    private final int qty;
+
+    public CartItem(Product product, int qty) {
+        this.product = product;
+        this.qty = qty;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public double total(){
+        return product.getPrice() * qty;
+    }
+
+}
