ffd26cc8f4d71222c05e4343d8c559919bbd4216
Stil98 <sabamilan98@gmail.com>
2021-07-03 11:23:55 +0200

    gClienti.jsp

--- src/main/java/progetto/SpaceGaming/http/CrmServlet.java
+++ src/main/java/progetto/SpaceGaming/http/CrmServlet.java
@@ -1,11 +1,13 @@
 package progetto.SpaceGaming.http;
 
+import progetto.SpaceGaming.utente.Utente;
 import progetto.SpaceGaming.utente.UtenteDAO;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "CrmServlet", value = "/crm/*")
 public class CrmServlet extends HttpServlet {
@@ -17,12 +19,32 @@ public class CrmServlet extends HttpServlet {
             case "/":
                 break;
             case "/dashboard":
-                UtenteDAO dao=new UtenteDAO();
-                int uTotali= dao.userCount();
+                // UtenteDAO dao=new UtenteDAO();
+                // int uTotali= dao.userCount();
                 request.getRequestDispatcher("/WEB-INF/views/crm/dashboard.jsp").forward(request, response);
                 break;
-            default:
+            case "/gClienti":
+                ArrayList<Utente> list= new ArrayList<>();
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+                list.add(new Utente("Gennaro", "Rascato", "Via abito a torre", "gennarorascato@gmail.com", "gens", "kjkjkjkj", "347322434", false, null));
+
+                request.setAttribute("customers", list);
+                request.getRequestDispatcher("/WEB-INF/views/crm/gClienti.jsp").forward(request, response);
                 break;
+            default:
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "Pagina non Trovata");
         }
     }
 
